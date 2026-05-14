b98ffe723d0dd35de5969afe269ba9d7cf32f82c
Hua Van Truong <vantruongday.2006@gmail.com>
2026-05-14 10:07:21 +0700

    add feat view detail in vehicle panel

--- src/main/java/com/htt/vehiclerental/dto/UpcomingRentalCustomer.java
+++ src/main/java/com/htt/vehiclerental/dto/UpcomingRentalCustomer.java
@@ -1,44 +1,45 @@
 package com.htt.vehiclerental.dto;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public class UpcomingRentalCustomer {
-    private String cccd;
-    private String name;
-    private String phone;
+    private String identityNumber;
+    private String fullName;
+    private String phoneNumber;
     private LocalDateTime startTime;
     private LocalDateTime expectedReturnTime;
 
-    public UpcomingRentalCustomer(String cccd, String name, String phone, LocalDateTime startTime, LocalDateTime expectedReturnTime) {
-        this.cccd = cccd;
-        this.name = name;
-        this.phone = phone;
+    public UpcomingRentalCustomer(String identityNumber, String fullName, String phoneNumber, LocalDateTime startTime, LocalDateTime expectedReturnTime) {
+        this.identityNumber = identityNumber;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
         this.startTime = startTime;
         this.expectedReturnTime = expectedReturnTime;
     }
 
-    public String getCccd() {
-        return cccd;
+    public String getIdentityNumber() {
+        return identityNumber;
     }
 
-    public void setCccd(String cccd) {
-        this.cccd = cccd;
+    public void setIdentityNumber(String identityNumber) {
+        this.identityNumber = identityNumber;
     }
 
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public LocalDateTime getStartTime() {
@@ -55,5 +56,26 @@ public class UpcomingRentalCustomer {
 
     public void setExpectedReturnTime(LocalDateTime expectedReturnTime) {
         this.expectedReturnTime = expectedReturnTime;
+    }
+
+    public static UpcomingRentalCustomer fromMap(Map<String, Object> map) {
+        return new UpcomingRentalCustomer(
+                (String) map.get("identityNumber"),
+                (String) map.get("fullName"),
+                (String) map.get("phoneNumber"),
+                (LocalDateTime) map.get("startTime"),
+                (LocalDateTime) map.get("expectedReturnTime")
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "UpcomingRentalCustomer{" +
+                "identityNumber='" + identityNumber + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", startTime=" + startTime +
+                ", expectedReturnTime=" + expectedReturnTime +
+                '}';
     }
 }
