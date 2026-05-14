b9ce578e145c829fd8a687fb27e36f205ee45b2c
Perfect2030 <Perfectmchypi@gmail.com>
2026-05-14 23:57:15 +0700

    finalizing

--- src/main/java/com/htt/vehiclerental/dal/DBHelper.java
+++ src/main/java/com/htt/vehiclerental/dal/DBHelper.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -48,38 +47,6 @@ public class DBHelper {
         } catch (SQLException e) {
             System.out.print("Database error! \nMessage: " + e.getMessage());
         }
-        return -1;
-    }
-
-    public int executeUpdateAndGetId(String sql, Object... params) {
-        try {
-            Connection connection = DriverManager.getConnection(connectionString, username, password);
-            PreparedStatement stm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-            for (int i = 0; i < params.length; i++)
-                if (params[i] == null) stm.setNull(i + 1, Types.NULL);
-                else stm.setObject(i + 1, params[i]);
-
-            int affectedRows = stm.executeUpdate();
-            if (affectedRows == 0) {
-                throw new SQLException("Creating record failed, no rows affected.");
-            }
-
-            ResultSet generatedKeys = stm.getGeneratedKeys();
-            int id = -1;
-            if (generatedKeys.next()) {
-                id = generatedKeys.getInt(1);
-            } else {
-                throw new SQLException("Creating record failed, no ID obtained.");
-            }
-
-            stm.close();
-            connection.close();
-            return id;
-        } catch (SQLException e) {
-            System.out.print("Database error! \nMessage: " + e.getMessage());
-        }
-
         return -1;
     }
 
